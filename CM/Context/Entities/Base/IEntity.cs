6ebfcaa0c16b545c057f696023866859458045c5
Victor Villavicencio <48601784+Led-Zeff@users.noreply.github.com>
2019-03-15 16:08:36 -0600

    Add files via upload

---
+++ CM/Context/Entities/Base/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.Context.Entities.Base
+{
+    interface IEntity
+    {
+        [DataType(DataType.DateTime)]
+        DateTime RegistrationDate { get; set; }
+        [DataType(DataType.DateTime)]
+        DateTime? ModificationDate { get; set; }
+    }
+}
