408e16ec3a4f3b1f433f28991b365332c70535f1
Victor Villavicencio <48601784+Led-Zeff@users.noreply.github.com>
2019-03-15 16:06:45 -0600

    Add files via upload

---
+++ CM/CasContext.cs
@@ -0,0 +1,20 @@
+﻿using CM.Context.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CM.Context
+{
+    class CasContext : DbContext
+    {
+        public CasContext() : base("CAS") { }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Cas> Cases { get; set; }
+        public DbSet<CasFile> CasFiles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
