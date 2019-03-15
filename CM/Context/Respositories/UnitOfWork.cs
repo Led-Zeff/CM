5023dcb3d63909699f8c5d6e7edaf8da4c440e78
Victor Villavicencio <48601784+Led-Zeff@users.noreply.github.com>
2019-03-15 16:11:32 -0600

    Add files via upload

---
+++ CM/Context/Respositories/UnitOfWork.cs
@@ -0,0 +1,73 @@
+﻿using CM.Context.Entities;
+using CM.Context.Repositories.Base;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CM.Context.Repositories
+{
+    class UnitOfWork : IDisposable
+    {
+        private CasContext context = new CasContext();
+        private BaseRepository<Cas> casRepository;
+        private BaseRepository<Project> projectRepository;
+        private BaseRepository<CasFile> casFileRepository;
+
+        public BaseRepository<Cas> CasRepository
+        {
+            get
+            {
+                if (casRepository == null)
+                    casRepository = new BaseRepository<Cas>(context);
+                return casRepository;
+            }
+        }
+
+        public BaseRepository<Project> ProjectRepository
+        {
+            get
+            {
+                if (projectRepository == null)
+                    projectRepository = new BaseRepository<Project>(context);
+                return projectRepository;
+            }
+        }
+
+        public BaseRepository<CasFile> CasFileRepository
+        {
+            get
+            {
+                if (casFileRepository == null)
+                    casFileRepository = new BaseRepository<CasFile>(context);
+                return casFileRepository;
+            }
+        }
+
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed && disposing)
+            {
+                context.Dispose();
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
