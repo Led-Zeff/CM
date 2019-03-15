56e1d1d3b9cc134f7315ba8df890199a9f346017
Victor Villavicencio <48601784+Led-Zeff@users.noreply.github.com>
2019-03-15 16:07:52 -0600

    Add files via upload

---
+++ CM/Context/BaseEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace CM.Context.Entities.Base
+{
+    abstract class BaseEntity : INotifyPropertyChanged
+    {
+        [DataType(DataType.DateTime)]
+        public DateTime RegistrationDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime? ModificationDate { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string propName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public void SetValue<T>(ref T field, T value, [CallerMemberName] string propName = "")
+        {
+            if (!object.Equals(field, value))
+            {
+                field = value;
+                NotifyPropertyChanged(propName);
+            }
+        }
+    }
+}
